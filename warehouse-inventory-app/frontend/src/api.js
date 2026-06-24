0861c42a76ea5f24418bb79b883e8e56c1a2921f
RoninFiregate <drago.smok12@onet.pl>
2026-06-24 18:40:46 +0200

    Update api.js

--- warehouse-inventory-app/frontend/src/api.js
+++ warehouse-inventory-app/frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 import { API_URL } from './config';
-const api = axios.create({ baseURL: API_URL });
+const api = axios.create({ baseURL: `${API_URL}/api` });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
